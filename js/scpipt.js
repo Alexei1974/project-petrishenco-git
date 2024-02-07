ab93242e674d0aa8dfa34a87aab0d268d67434d9
Alexej <exemp@mail.com>
2024-02-07 13:10:03 +0300

    first commit

---
+++ js/scpipt.js
@@ -0,0 +1,39 @@
+
+"use strict";
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50,
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(userName);
+// var userName = 'bob';
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5%3);
+
+// console.log(2*4 === '8');
+
+const isChecked = false,
+    isClose = false;
+
+    // console.log(isChecked && isClose);
+    console.log(isChecked || !isClose);
+
+    console.log(2 + 2 * 2 !== +'6');
